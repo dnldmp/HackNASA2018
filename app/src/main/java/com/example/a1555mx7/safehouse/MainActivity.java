0886384df42c174c6840c341c662e133302db08a
dnldmp <Danilodomp@gmail.com>
2018-10-20 23:04:13 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/a1555mx7/safehouse/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1555mx7.safehouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
